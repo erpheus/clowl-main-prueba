4243c9c971d868127ef2614b1d956abea3e1b183
Clowl Admin <admin@clowl.org>
2012-06-02 22:01:39 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<head>
+	<title>Clowl</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<style>
+	#logo {
+	    text-align:center;
+	    display:table-cell;
+	    vertical-align:middle;
+	}
+	body{
+		display: table;
+		height:100%;
+		width:100%;
+	}
+	</style>
+</head>
+<body>
+	<div id="logo">
+		<img src="images/clowl-nombre-512.png" />
+	</div>
+</body>
